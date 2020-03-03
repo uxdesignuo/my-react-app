296118bb1e81292c907112ea4f7137b2a8ffeca2
uo <schoolgirluche@gmail.com>
2020-03-02 23:52:10 -0800

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and Code React</h1>
     <p>
-      Complete cources about the best tools and design systems. Prototypes a and
+      Complete courses about the best tools and design systems. Prototypes a and
       build apps with React and Swift.
     </p>
     <p>Now I'm gonna build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video!</Link>
   </div>
 )
 
