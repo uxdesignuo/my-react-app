afa8656ad5042b58d1d76d20bb942e25d6e0ae37
uo <schoolgirluche@gmail.com>
2020-03-09 21:07:04 -0700

    code up to stateful

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop >= 50) {
+      this.setState({
+        hasScrolled: true,
+      })
+    } else {
+      this.setState({
+        hasScrolled: false,
+      })
+    }
+  }
+
+  render() {
+    /* must be a function */
+    return (
+      <div
+        className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={require('../images/logo-designcode.svg')} width="30" />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
